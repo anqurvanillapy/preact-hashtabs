dcb4d486431e7590154b5cbf1c376a4d8761536c
anqurvanillapy <luxingyu@kingsoft.com>
2017-08-10 19:52:00 +0800

    updated w/ & w/o anchor examples

--- index.js
+++ index.js
@@ -2,23 +2,65 @@ import { h, render, Component } from 'preact'
 import Router from 'preact-router'
 import { Link } from 'preact-router/match'
 
-class App extends Component {
+class WithoutAnchor extends Component { // eslint-disable-line no-unused-vars
   render () {
     return (
       <main>
         <ul>
-          <li><Link activeClassName='active' href='/'>home</Link></li>
-          <li><Link activeClassName='active' href='/foo'>foo</Link></li>
-          <li><Link activeClassName='active' href='/bar'>bar</Link></li>
+          <li><Link activeClassName='active' href='/'>Home</Link></li>
+          <li><Link activeClassName='active' href='/about'>About</Link></li>
         </ul>
+        <hr />
         <Router>
-          <p path='/'>home</p>
-          <p path='/foo'>foo</p>
-          <p path='/bar'>bar</p>
+          <section path='/:tab?/:subtab?'>
+            <p>contents</p>
+            <ul>
+              <li><Link activeClassName='active' href='/'>post2</Link></li>
+              <li><Link activeClassName='active' href='/post/1'>post1</Link></li>
+              <li><Link activeClassName='active' href='/post/0'>post0</Link></li>
+            </ul>
+            <Router>
+              <p path='/'>day 3: fine</p>
+              <p path='/post/1'>day 2: okay</p>
+              <p path='/post/0'>day 1: good</p>
+            </Router>
+          </section>
+          <p path='/about'>nothing's real.</p>
         </Router>
       </main>
     )
   }
 }
 
-render(<App />, document.body)
+class WithAnchor extends Component {
+  render () {
+    return (
+      <main>
+        <ul>
+          <li><Link activeClassName='active' href='/'>Home</Link></li>
+          <li><Link activeClassName='active' href='/about'>About</Link></li>
+        </ul>
+        <hr />
+        <Router>
+          <section path='/'>
+            <p>contents</p>
+            <ul>
+              <li><Link activeClassName='active' href='/'>post2</Link></li>
+              <li><Link activeClassName='active' href='/#post1'>post1</Link></li>
+              <li><Link activeClassName='active' href='/#post0'>post0</Link></li>
+            </ul>
+            <Router>
+              <p path='/'>day 3: fine</p>
+              <p path='/#post1'>day 2: okay</p>
+              <p path='/#post0'>day 1: good</p>
+            </Router>
+          </section>
+          <p path='/about'>nothing's real.</p>
+        </Router>
+      </main>
+    )
+  }
+}
+
+// render(<WithoutAnchor />, document.body)
+render(<WithAnchor />, document.body) // currently not working
